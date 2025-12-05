9a3951605ea3a7b395348cec846f3d091828f5cd
janvee933 <janvee841301@gmail.com>
2025-12-05 22:14:45 +0530

    Add loader component and splash screen

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,22 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Cinemas from './pages/Cinemas';
 import ComingSoon from './pages/ComingSoon';
+import Loader from './components/Loader';
 
 function App() {
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <div className="app">
       <Navbar />
@@ -25,6 +39,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/cinemas" element={<Cinemas />} />
           <Route path="/offers" element={<ComingSoon title="Offers" />} />
+          <Route path="/loading" element={<Loader />} />
         </Routes>
       </main>
       <Footer />
