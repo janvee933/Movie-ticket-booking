abe39c21e30796c22c48a93358dd9ed5a53109a7
Janvee Sharma <janveesharma209@gmail.com>
2026-01-20 00:58:12 +0530

    Cleanup debug scripts

--- check_root_env.js
+++
@@ -1,3 +0,0 @@
-import 'dotenv/config';
-console.log('Current Directory:', process.cwd());
-console.log('MONGO_URI:', process.env.MONGO_URI ? 'Defined' : 'Undefined');
--- server/debug_env.js
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config';
-console.log('Current Directory:', process.cwd());
-console.log('MONGO_URI:', process.env.MONGO_URI ? 'Defined' : 'Undefined');
-if (process.env.MONGO_URI) {
-    console.log('MONGO_URI Length:', process.env.MONGO_URI.length);
-}
-// Do not print the actual value for security
